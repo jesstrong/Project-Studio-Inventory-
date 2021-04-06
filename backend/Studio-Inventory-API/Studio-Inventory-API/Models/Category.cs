edbbb1b173fada96efa3aa9da445871cb2a04283
Gav Hensley <gavinrhensley@gmail.com>
2021-04-06 12:13:10 -0400

    added the category model along with its properties and constructors

---
+++ backend/Studio-Inventory-API/Studio-Inventory-API/Models/Category.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Studio_Inventory_API.Models
+{
+    public class Category
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public virtual ICollection<Equipment> EquipmentList { get; set; }
+
+        public Category()
+        {
+
+        }
+
+        public Category(int id, string name)
+        {
+            Id = id;
+            Name = name;
+        }
+    }
+}
