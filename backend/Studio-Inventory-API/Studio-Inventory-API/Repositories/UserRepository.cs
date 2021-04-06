ff3e58f1da8dc934b9f178f22b9e992e4d93f33e
JES Strong <janetestrong@yahoo.com>
2021-04-06 15:00:01 -0400

    set up Repositories for Cat, Equip, IRep, UserRep and DepI

---
+++ backend/Studio-Inventory-API/Studio-Inventory-API/Repositories/UserRepository.cs
@@ -0,0 +1,17 @@
+﻿using Studio_Inventory_API.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Studio_Inventory_API.Repositories
+{
+    public class UserRepository : Repository<User>, IRepository<User>
+    {
+        public UserRepository(StudioContext context) : base(context)
+        {
+
+        }
+
+    }
+}
