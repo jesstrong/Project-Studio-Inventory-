997d77265012a6805e3487ef2c1d094664c79503
Joseph Erxleben <jerxleben.13@gmail.com>
2021-04-08 15:05:10 -0400

    second push because I forgot to save last change.

--- backend/Studio-Inventory-API/Studio-Inventory-API/Repositories/IRepository.cs
+++ backend/Studio-Inventory-API/Studio-Inventory-API/Repositories/IRepository.cs
@@ -2,11 +2,12 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Studio_Inventory_API.Extensions;
 using Studio_Inventory_API.Models; 
 
 namespace Studio_Inventory_API.Repositories
 {
-    public interface IRepository<T> where T : class
+    public interface IRepository<T>: IUser where T : class
     {
         IEnumerable<T> GetAll();
         T GetById(int id);
