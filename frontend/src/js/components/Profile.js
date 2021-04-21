0b596118cb32374586ad5d6a7dcd1f159f41486f
Joseph Erxleben <jerxleben.13@gmail.com>
2021-04-21 14:49:48 -0400

    User can now change username and/or password, but cannot use a pre-existing username.

--- frontend/src/js/components/Profile.js
+++ frontend/src/js/components/Profile.js
@@ -6,7 +6,8 @@ const appDiv = document.getElementById('app');
 export default{
     ProfilePage,
     NavUserProfile,
-    CancelRentalRequest
+    CancelRentalRequest,
+    UpdateProfileButton
 }
 
 function ProfilePage(User){
@@ -18,6 +19,8 @@ function ProfilePage(User){
     return `
     </br>
     <h3>Welcome ${User.name}</h3>
+    <button class="updateProfileBtn" id="${User.id}">Update Profile</button>
+    </br>
     <h4>Here is a record of your rentals:</h4>
     <ul>
     ${User.rentals.map(rental =>{
@@ -57,6 +60,7 @@ function NavUserProfile(userId){
         appDiv.innerHTML = ProfilePage(user);
         Rental.RentalDetailsButton();
         CancelRentalRequest();
+        UpdateProfileButton();
     })
 }
 
@@ -75,3 +79,99 @@ function CancelRentalRequest(){
     })
 }
 
+function UpdateProfileButton(){
+    const updateProfileElement = document.querySelector('.updateProfileBtn')
+    updateProfileElement.addEventListener('click', function(){
+        const userId = updateProfileElement.id;
+        apiAction.getRequest(`https://localhost:44372/api/User/${userId}`, user => {
+            appDiv.innerHTML = UpdateProfilePage(user);
+            UpdateUserButton(user);
+        })
+    })
+}
+
+function UpdateProfilePage(data){
+    return`
+        <section class='updateProfileForm'>
+            </br>
+            <input type='text' id='userName' value='${data.name}' />
+            <br/>
+            <input type='password' id='newPassword' placeholder='New Password'/>
+            <br/>
+            <input type='password' id='oldPassword' placeholder='Old Password'/>
+            <br/>
+            <button id='updateUserButton'>Update Profile</button>
+            <div id="warningText"></div>
+        </section>
+    `
+}
+
+function UpdateUserButton(data){
+    const updateUserElement = document.getElementById('updateUserButton');
+    updateUserElement.addEventListener('click', function(){
+        const oldUserName = data.name;
+        const oldPassword = document.getElementById('oldPassword').value;
+        var newUserName = document.getElementById('userName').value;
+        var newPassword = document.getElementById('newPassword').value;
+        const userId = data.id;
+        const userIsAdmin = data.isAdmin;
+
+        const userCheck = {
+            Name: oldUserName,
+            Password: oldPassword
+        }
+
+        var isUnique = true;
+        
+        
+        apiAction.postRequest('https://localhost:44372/api/Account', userCheck, data =>{
+            if(data.result == true){
+                if(newUserName === ""){
+                    newUserName = oldUserName;
+                }
+                if(newPassword === ""){
+                    newPassword = oldPassword;
+                }
+
+                const requestBody = {
+                    Id: userId,
+                    Name: newUserName,
+                    Password: newPassword,
+                    IsAdmin: userIsAdmin
+                }
+
+                if(newUserName != oldUserName){
+                    apiAction.getRequest('https://localhost:44372/api/User', users =>{
+                        users.forEach(user => {
+                            if (user.name === newUserName){
+                                isUnique = false;
+                            }
+                        });
+
+                        if(isUnique == true){
+                            UpdateProfile(requestBody, userId);
+                        }
+                        else{
+                            const warningElement = document.getElementById('warningText');
+                            warningElement.innerHTML = "That User Name already exists.";
+                        }
+                    })
+                }   
+                else{
+                    UpdateProfile(requestBody, userId);
+                }
+ 
+            }
+            else{
+                const warningElement = document.getElementById('warningText');
+                warningElement.innerHTML = data.message;
+            }
+        })
+    })
+}
+
+function UpdateProfile(requestBody, id){
+    apiAction.putRequest('https://localhost:44372/api/User/', id, requestBody, user =>{
+        appDiv.innerHTML = "Your profile has been updated";
+    })
+}
